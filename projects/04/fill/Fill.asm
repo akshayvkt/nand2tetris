7ca3e14fc529fad724847b1cc04cf57b710010c7
Akshay Chintalapati <64036106+akshayvkt@users.noreply.github.com>
2023-11-07 21:03:37 -0800

    fill in progress

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,58 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+
+@R1
+M = 8192
+
+
+@i
+M = 1
+
+
+(START)
+    @i
+    D = M 
+    @Screen
+    A = A + D
+    M = 0
+
+
+    @Keyboard
+    D = M
+    @LOOP
+    D; JGT //if the keyboard value is >0, then go back to the loop
+
+    @i 
+    M = M + 1
+
+    @R1
+    @START
+    D; JMP //if the keyboard value is zero, continue through the start loop
+
+
+
+(LOOP)
+    @Keyboard
+    D = M
+    @START   // if Keyboard == 0, then jump back to Start, which loops through
+    D; JMP   // and assigns each element's memory to zero value
+    @i 
+    D = M
+    M = M + 1
+    @Screen
+    A = A + D
+    M = -1
+
+    @R1
+    D = M 
+    @i
+    D = M - D 
+    @END
+    D; JMP 
+
+
+(END)
+    @END
+    0; JMP
